519728323612a5dbad9fe7cc7e6467f232bdda0f
Gh0st-arch <72387090+Gh0st-arch@users.noreply.github.com>
2020-10-24 16:09:06 +0800

    Add files via upload

---
+++ People.java
@@ -0,0 +1,18 @@
+package school;
+
+class People 
+{
+	int number;
+	String name,sex;
+	People(int number ,String name,String sex)
+	{
+		this.number = number;
+		this.name = name;
+		this.sex = sex;
+	}
+	void show()
+	{
+		
+	}
+  
+}
